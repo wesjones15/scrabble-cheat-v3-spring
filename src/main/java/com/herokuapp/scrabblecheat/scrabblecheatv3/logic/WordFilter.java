ca41a5f71116d8001607addf79077a111821a309
Wes Jones <wesjones@Wess-MacBook-Pro.local>
2020-01-22 13:03:34 -0500

    WordFilter business logic now works. blank tile support added

--- src/main/java/com/herokuapp/scrabblecheat/scrabblecheatv3/logic/WordFilter.java
+++ src/main/java/com/herokuapp/scrabblecheat/scrabblecheatv3/logic/WordFilter.java
@@ -29,20 +29,80 @@ public class WordFilter {
     }
 
     public ArrayList<String> filterWordsByUnusedLetters(ArrayList<String> words, String letters) {
+        ArrayList<String> unused = buildListOfUnusedLetters(letters);
+        ArrayList<String> filtered = new ArrayList<>();
+        for (String word : words) {
+            int underscoreCount = getUnderScoreCount(letters);
+            int unusedCount = 0;
+            for (String letter : unused) {
+                if (word.contains(letter)) {
+                    unusedCount++;
+                }
+            }
+            if (unusedCount <= underscoreCount)
+                filtered.add(word);
+        }
         // if _ present in letters
         //      for each word:
         //          track number of unused letters present in word
         //          if UL count <= (# of _)
         //              keep word
-        return null;
+        return filtered;
+    }
+
+    public Integer getUnderScoreCount(String letters) {
+        return getCharCount(letters, '_');
+    }
+
+    public Integer getCharCount(String input, Character ch) {
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            Character letter = input.charAt(i);
+            if (letter.equals(ch)) count++;
+        }
+        return count;
     }
 
     public ArrayList<String> filterWordsByLetterFrequency(ArrayList<String> words, String letters) {
+        ArrayList<String> filtered = new ArrayList<>();
+        Integer underscoreCount = getUnderScoreCount(letters);
+        String uniqueLetters = getUniqueLetters(letters);
+
+        for (String word : words) {
+            int delta = 0;
+            String sharedLetters = getUniqueLettersSharedBetweenQueryAndWord(uniqueLetters, word);
+            for (int i = 0; i < sharedLetters.length(); i++) {
+                Character letter = sharedLetters.charAt(i);
+                int countInWord = getCharCount(word, letter);
+                int countInQuery = getCharCount(letters, letter);
+                delta += (countInQuery < countInWord) ? (countInWord - countInQuery) : 0;
+            }
+            // MAKE JUDGEMENT AND ADD OR EXCLUDE WORD
+            if (delta <= underscoreCount) filtered.add(word);
+
+        }
+
+
         // if _ present in letters
         //      for each word:
         //          track delta of extra unused letters
         //          keep word if delta - (# of _) == 0;
-        return null;
+        return filtered;
+    }
+
+    public String getUniqueLetters(String letters) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < letters.length(); i++) {
+            String letter = String.valueOf(letters.charAt(i));
+            if (!sb.toString().contains(letter) && letter.matches("[a-zA-Z]"))
+                sb.append(letter);
+        }
+//        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public String getUniqueLettersSharedBetweenQueryAndWord(String query, String word) {
+        return getUniqueLetters(query+word);
     }
 
     public ArrayList<String> getScrabbleWordList() {
--- src/test/java/com/herokuapp/scrabblecheat/scrabblecheatv3/logic/TestWordFilter.java
+++ src/test/java/com/herokuapp/scrabblecheat/scrabblecheatv3/logic/TestWordFilter.java
@@ -9,6 +9,27 @@ import java.util.List;
 
 public class TestWordFilter {
     @Test
+    public void testGetScrabbleWordsFromLetters() {
+        WordFilter wordFilter = new WordFilter();
+        String letters = "WHISKEY";
+        ArrayList<String> out = wordFilter.getScrabbleWordsFromLetters(letters);
+        for (int i = 0; i < out.size(); i++) {
+            System.out.println(out.get(i));
+        }
+    }
+
+    @Test
+    public void testGetScrabbleWordsFromLetters2() {
+        WordFilter wordFilter = new WordFilter();
+        String letters = "APP_DE";
+        ArrayList<String> out = wordFilter.getScrabbleWordsFromLetters(letters);
+        for (int i = 0; i < out.size(); i++) {
+            System.out.println(out.get(i));
+        }
+    }
+
+
+    @Test
     public void testGetScrabbleWordList1() {
         WordFilter wordFilter = new WordFilter();
         ArrayList<String> words = wordFilter.getScrabbleWordList();
@@ -58,14 +79,80 @@ public class TestWordFilter {
         WordFilter wordFilter = new WordFilter();
         String letters = "JBIRRD";
         ArrayList<String> words = getMockWords();
-        Integer expected = 5;
+        Integer expected = 4;
         Integer actual = wordFilter.filterWordsByLength(words, letters).size();
         Assert.assertEquals(expected, actual);
     }
 
-    private ArrayList<String> getMockWords() {
-        String[] mockWords = {"GASTROINTESTINAL", "JAGUAR", "TRICYCLE", "LONG", "ASK", "REPTILE", "FLAG", "POOL"};
-        return new ArrayList<>(Arrays.asList(mockWords));
+    @Test
+    public void testFilterWordsByUnusedLetters() {
+        WordFilter wordFilter = new WordFilter();
+        ArrayList<String> words = getMockWords();
+        String letters = "SAFD";
+        String[] expected = {"A", "AAS", "ASDF"};
+        ArrayList<String> actual = wordFilter.filterWordsByUnusedLetters(words, letters);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i], actual.get(i));
+        }
     }
 
+    @Test
+    public void testFilterWordsByUnusedLettersUnderscore() {
+        WordFilter wordFilter = new WordFilter();
+        ArrayList<String> words = arrayListOf("SAFE","SDFS","LLOK","SLAP","FLAG");
+        String letters = "SAF_";
+        String[] expected = {"SAFE","SDFS"};
+        ArrayList<String> actual = wordFilter.filterWordsByUnusedLetters(words, letters);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertTrue(actual.contains(expected[i]));
+        }
+    }
+
+    @Test
+    public void testFilterWordsByLetterFrequency() {
+        WordFilter wordFilter = new WordFilter();
+        ArrayList<String> words = arrayListOf("A","AA","AAA","AAAA","AAAAA");
+        String letters = "AAA";
+        String[] expected = {"A","AA","AAA"};
+        ArrayList<String> actual = wordFilter.filterWordsByLetterFrequency(words, letters);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertTrue(actual.contains(expected[i]));
+        }
+    }
+
+    @Test
+    public void testFilterWordsByLetterFrequencyWithUnderscore() {
+        WordFilter wordFilter = new WordFilter();
+        ArrayList<String> words = arrayListOf("A","AAF","AAA","AAAA","AAAAA");
+        String letters = "AA_";
+        String[] expected = {"A","AAF","AAA"};
+        ArrayList<String> actual = wordFilter.filterWordsByLetterFrequency(words, letters);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertTrue(actual.contains(expected[i]));
+        }
+        Assert.assertEquals(expected.length, actual.size());
+    }
+
+    @Test
+    public void testFilterWordsByLetterFrequencyWithUnderscore2() {
+        WordFilter wordFilter = new WordFilter();
+        String letters = "APP_DE";
+        ArrayList<String> words = arrayListOf("APPA","PAPA","APP","AAAF","ADAPT","ADDED","AAPDED","DEEP","PAD","DIP","DIPPA");
+        ArrayList<String> expected = arrayListOf("APPA","PAPA","APP","DEEP","PAD","DIP","DIPPA");
+        ArrayList<String> actual = wordFilter.filterWordsByLetterFrequency(words, letters);
+        System.out.println("expected\tactual");
+        System.out.println(expected.size() + "\t\t" + actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            System.out.println(expected.get(i) + "\t\t" + actual.get(i));
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    private ArrayList<String> getMockWords() {
+        return arrayListOf("A", "AAS","ASDF","ASDFG","ASFGHFD");
+    }
+
+    private ArrayList<String> arrayListOf(String... words) {
+        return new ArrayList<>(Arrays.asList(words));
+    }
 }
